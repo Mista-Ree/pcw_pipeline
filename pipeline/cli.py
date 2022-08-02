b84ad8855a553f590871556cf0a05b0fe3b0f037
Mista-Ree <chinmaykamat@hotmail.com>
2022-08-02 20:32:15 +0100

    Use defined CSV constants.

--- pipeline/cli.py
+++ pipeline/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from pipeline.pipeline import ingest, run_pipeline, transform, validate, evaluate
+from pipeline.pipeline_config import CLEAN_CSV, INPUT_CSV
 
 @click.group()
 def cli():
@@ -9,8 +10,8 @@ def cli():
 @cli.command(help="Runs the full pipeline")
 @click.option("--print-transform-output", is_flag=True, default=False, help="Flag to specify whether the output of transforms should be printed to the console")
 @click.option("--print-evaluation-output", is_flag=True, default=False, help="Flag to specify whether the output of model evaluation should be printed to the console")
-@click.option("--input-csv", default="quotes.csv.gz", help="Path to input csv")
-@click.option("--output-csv", default="clean_quotes.csv.gz", help="Path to output csv")
+@click.option("--input-csv", default=INPUT_CSV, help="Path to input csv")
+@click.option("--output-csv", default=CLEAN_CSV, help="Path to output csv")
 def run_full(
     print_transform_output: bool,
     print_evaluation_output: bool,
@@ -25,24 +26,24 @@ def run_full(
     )
 
 @cli.command(name="ingest")
-@click.option("--input-csv", default="quotes.csv.gz", help="Path to input csv")
-@click.option("--output-csv", default="clean_quotes.csv.gz", help="Path to output csv")
+@click.option("--input-csv", default=INPUT_CSV, help="Path to input csv")
+@click.option("--output-csv", default=CLEAN_CSV, help="Path to output csv")
 def run_ingest(input_csv: str, output_csv: str):
     ingest(raw_data=input_csv, output_csv_path=output_csv)
 
 @cli.command(name="validate")
-@click.option("--input-csv", default="clean_quotes.csv.gz", help="Path to input csv")
+@click.option("--input-csv", default=CLEAN_CSV, help="Path to input csv")
 def run_validate(input_csv: str):
     validate(input_csv)
 
 @cli.command(name="transform")
-@click.option("--input-csv", default="clean_quotes.csv.gz", help="Path to input csv")
+@click.option("--input-csv", default=CLEAN_CSV, help="Path to input csv")
 @click.option("--print-transform-output", is_flag=True, default=False, help="Flag to specify whether the output of transforms should be printed to the console")
 def run_transform(input_csv: str, print_transform_output: bool):
     transform(input_csv, print_transform_output=print_transform_output)
 
 @cli.command(name="evaluate")
-@click.option("--input-csv", default="clean_quotes.csv.gz", help="Path to input csv")
+@click.option("--input-csv", default=CLEAN_CSV, help="Path to input csv")
 @click.option("--print-evaluation-output", is_flag=True, default=False, help="Flag to specify whether the output of model evaluation should be printed to the console")
 def run_evaluate(input_csv: str, print_evaluation_output: bool):
     evaluate(input_csv, print_evaluation_output=print_evaluation_output)
